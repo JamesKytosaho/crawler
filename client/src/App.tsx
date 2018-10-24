27fdd15367000a660b83a1c5c2993b00d482b4ee
Kytösaho James <james.kytosaho@afconsult.com>
2018-10-24 16:28:53 +0200

    Typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public state = {
     response: ''
   };
 
-  componentDidMount() {
+  public componentDidMount() {
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
   }
 
-  callApi = async () => {
+  public callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
 
-    if (response.status !== 200) throw Error(body.message);
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    else {
+      console.log("response", body);
+    }
 
     return body;
   };
   
-  render() {
+
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <p className="App-intro">{this.state.response}</p>
       </div>
     );
