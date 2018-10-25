98e2367207b28e76b11a738ee96b4c859ffd623a
Kytösaho James <james.kytosaho@afconsult.com>
2018-10-25 16:04:15 +0200

    react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import logo from './logo.svg';
+
+const ExampleComponent = () => <h2 />;
+const ExampleComponent1 = () => <h2>Standalone example page</h2>;
+const ExampleComponent2 = () => <h2>Group of pages</h2>;
 
 class App extends React.Component {
   public state = {
@@ -31,16 +37,30 @@ class App extends React.Component {
 
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
+      <Router>
+        <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <p className="App-intro">{this.state.response}</p>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/1">Example route 1</Link></li>
+              <li><Link to="/2">Example route 2</Link></li>
+            </ul>
+            <div>
+              <Switch>
+                  <Route exact path="/" component={ExampleComponent} />
+                  <Route exact path="/1" component={ExampleComponent1} />
+                  <Route exact path="/2" component={ExampleComponent2} />
+              </Switch>
+            </div>
+          </div>
+      </Router>
     );
   }
 }
